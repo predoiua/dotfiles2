bb77c6ef1d9baeb967cfe9987c6098647accd9e4
predoiua <predoiua@vv10.eu>
2025-11-05 08:17:56 +0200

    startify no headers

--- nvim/.config/nvim/lua/plugins/startify.lua
+++ nvim/.config/nvim/lua/plugins/startify.lua
@@ -1,3 +1,7 @@
 return {
     "mhinz/vim-startify",
+    config = function()
+        -- Set the custom header to an empty table to disable the cow/header
+        vim.g.startify_custom_header = {}
+    end,
 }
